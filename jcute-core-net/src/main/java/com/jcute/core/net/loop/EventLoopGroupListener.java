4a9377b9c9144de2b70e8da6630f24f239db7e2d
tb_bin@163.com <tb_bin@163.com>
2018-07-16 09:08:20 +0800

    重新梳理思路

--- jcute-core-net/src/main/java/com/jcute/core/net/loop/EventLoopGroupListener.java
+++
@@ -1,9 +0,0 @@
-package com.jcute.core.net.loop;
-
-import com.jcute.core.toolkit.cycle.StableListener;
-
-public interface EventLoopGroupListener extends StableListener<EventLoopGroup>{
-	
-	
-	
-}
